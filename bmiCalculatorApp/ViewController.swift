06222d374817563f46d806669fe4abe2022b5439
Joseph-GM <jkim.english75@gmail.com>
2021-11-25 08:57:03 +0900

    Initial Commit

---
+++ bmiCalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmiCalculatorApp
+//
+//  Created by Joseph Kim on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
